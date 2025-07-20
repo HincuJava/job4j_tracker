4fd4460729c8b404d80a2d835b2aa3cc39f038a6
Hincu.N <hincujava@gmail.com>
2025-07-20 10:31:40 +0300

    6.1. Разрыв зависимости StartUI от Scanner. REBORN [#181778]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,16 +1,13 @@
 package ru.job4j.tracker;
 
-
 public class StartUI {
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             showMenu();
-            System.out.print("Выбрать: ");
-            int select = Integer.parseInt(scanner.nextLine());
+            int select = Integer.parseInt(input.askStr("Chose: "));
             if (select == 0) {
                 System.out.println("=== Создание новой заявки ===");
-                System.out.print("Введите имя: ");
                 String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
@@ -27,10 +24,8 @@ public class StartUI {
                 }
             } else if (select == 2) {
                 System.out.println("=== Редактирование заявки ===");
-                System.out.print("Введите id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Введите имя: ");
-                String name = scanner.nextLine();
+                int id = Integer.parseInt(input.askStr("Enter id: "));
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
                     System.out.println("Заявка изменена успешно.");
@@ -40,13 +35,12 @@ public class StartUI {
             } else if (select == 3) {
                 System.out.println("=== Удаление заявки ===");
                 System.out.print("Введите id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = Integer.parseInt(input.askStr("Enter id: "));
                 Item item = tracker.findById(id);
                 tracker.delete(id);
                 System.out.println(item != null ? "Заявка удалена успешно." : "Ошибка удаления заявки.");
             } else if (select == 4) {
                 System.out.println("=== Вывод заявки по id ===");
-                System.out.print("Введите id: ");
                 int id = Integer.parseInt(input.askStr("Enter id: "));
                 Item item = tracker.findById(id);
                 if (item != null) {
@@ -56,8 +50,7 @@ public class StartUI {
                 }
             } else if (select == 5) {
             System.out.println("=== Вывод заявок по имени ===");
-            System.out.print("Введите имя: ");
-            String name = scanner.nextLine();
+            String name = input.askStr("Enter name: ");
             Item[] items = tracker.findByName(name);
             if (items.length > 0) {
                 for (Item item : items) {
