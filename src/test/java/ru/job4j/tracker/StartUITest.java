dc7ba8e36cbbe0a66ce45a346fd8f5f814d85217
Hincu.N <hincujava@gmail.com>
2025-07-22 17:30:02 +0300

    12.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.in;
 
 class StartUITest {
     @Test
@@ -70,4 +71,94 @@ class StartUITest {
                         + "=== Завершение программы ===" + System.lineSeparator()
         );
     }
-}
+
+    @Test
+    void whenReplaceItemTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input input = new MockInput(
+                new String[] {"0", String.valueOf(one.getId()), replaceName, "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new ReplaceAction(output),
+                new ExitAction(output)
+        };
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(output.toString()).isEqualTo(
+                "Меню:" + ln
+                        + "0. Изменить заявку" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Редактирование заявки ===" + ln
+                        + "Заявка изменена успешно." + ln
+                        + "Меню:" + ln
+                        + "0. Изменить заявку" + ln
+                        + "1. Завершить программу" + ln
+                        + "=== Завершение программы ===" + ln
+        );
+    }
+
+    @Test
+    void whenFindAllActionTestOutputIsSuccessfully() {
+        Output output = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test2"));
+
+        Input input = new MockInput(
+                new String[] {"0", "1"}
+        );
+
+        UserAction[] actions = new UserAction[] {
+                new FindAllAction(output),
+                new ExitAction(output)
+        };
+
+        new StartUI(output).init(input, tracker, actions);
+        String ln = System.lineSeparator();
+
+        assertThat(output.toString()).isEqualTo("Меню:" + ln
+                + "0. Показать все заявки" + ln
+                + "1. Завершить программу" + ln
+                + "=== Вывод всех заявок ===" + ln
+                + one + ln
+                + two + ln
+                + "Меню:" + ln
+                + "0. Показать все заявки" + ln
+                + "1. Завершить программу" + ln
+                + "=== Завершение программы ===" + ln
+        );
+        }
+
+        @Test
+        void whenFindByNameActionOutputIsSuccessfully() {
+            Output output = new StubOutput();
+            Tracker tracker = new Tracker();
+            Item one = tracker.add(new Item("test1"));
+
+            Input input = new MockInput(
+                    new String[] {"0","test1", "1"}
+            );
+
+            UserAction[] actions = new UserAction[] {
+                    new FindByNameAction(output),
+                    new ExitAction(output)
+            };
+
+            new StartUI(output).init(input, tracker, actions);
+            String ln = System.lineSeparator();
+            assertThat(output.toString()).isEqualTo(
+                    "Меню:" + ln
+                            + "0. Показать заявки по имени" + ln
+                            + "1. Завершить программу" + ln
+                            + "=== Вывод заявок по имени ===" + ln
+                            + one + ln
+                            + "Меню:" + ln
+                            + "0. Показать заявки по имени" + ln
+                            + "1. Завершить программу" + ln
+                            + "=== Завершение программы ===" + ln
+            );
+          }
+    }
