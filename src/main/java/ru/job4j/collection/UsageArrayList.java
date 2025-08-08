4abcc29f6b894f1b2a275b8ad1bb3b8d7190a8c0
Hincu.N <hincujava@gmail.com>
2025-08-08 15:59:43 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> name = new ArrayList<>();
+        name.add("Petr");
+        name.add("Ivan");
+        name.add("Stepan");
+        for (Object value : name) {
+            System.out.println(value);
+        }
+    }
+}
